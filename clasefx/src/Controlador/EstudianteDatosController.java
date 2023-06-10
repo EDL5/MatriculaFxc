91a2ab2165431667f338ea5eda0a5d64a956805a
Edu <74061627@continental.edu.pe>
2023-06-09 22:05:04 -0500

    parte grafica alumno

--- clasefx/src/Controlador/EstudianteDatosController.java
+++ clasefx/src/Controlador/EstudianteDatosController.java
@@ -1,26 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package Controlador;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Edu
- */
-public class EstudianteDatosController implements Initializable {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+import javafx.util.StringConverter;
 
-    /**
-     * Initializes the controller class.
-     */
+public class EstudianteDatosController implements Initializable{
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private ChoiceBox<String> chbGrado;
+
+    @FXML
+    private ChoiceBox<String> chbNivel;
+
+    @FXML
+    private ChoiceBox<String> chbSeccion;
+
+    @FXML
+    private ChoiceBox<String> chbSituacion;
+
+    @FXML
+    private DatePicker dapFechaNacimiento;
+
+    @FXML
+    private TextField txtApellidoMaterno;
+
+    @FXML
+    private TextField txtApellidoPaterno;
+
+    @FXML
+    private TextField txtCiudadNacimiento;
+
+    @FXML
+    private TextField txtCodigoEstudiantil;
+
+    @FXML
+    private TextField txtColegioProcedente;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtEdad;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtProvinciaNacimiento;
+
+    @FXML
+    void Guardar(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void DateFecha(ActionEvent event) {
+        LocalDate fechita = dapFechaNacimiento.getValue();
+        String pattern = "MMMM dd, yyyy";
+        String datePattern = fechita.format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+    private String[] gradito1 = {null,"1°","2°","3°","4°","5°"};
+    private String[] gradito2 = {null,"1°","2°","3°","4°","5°","6°"};
+    private String[] nivelito = {null,"Inicial","Primaria","Secundaria"};
+    private String[] secshion = {null,"A","B","C","D","E","F"};
+    private String[] situasau = {null,"1°","2°","3°","4°","5°","6°"};
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        chbGrado.getItems().addAll(gradito1);
+        chbNivel.getItems().addAll(nivelito);
+        chbSeccion.getItems().addAll(secshion);
+        chbSituacion.getItems().addAll(situasau);
+    }
+
 }
