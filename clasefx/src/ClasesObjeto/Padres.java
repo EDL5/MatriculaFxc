f4b884388faff80e8a80f264169707bf1072d83a
Edu <74061627@continental.edu.pe>
2023-06-12 15:51:30 -0500

    Objeto Padres echo

--- clasefx/src/ClasesObjeto/Padres.java
+++ clasefx/src/ClasesObjeto/Padres.java
@@ -1,5 +1,46 @@
 package ClasesObjeto;
 
-public class Padres {
+public class Padres extends Persona{
+    
+    private String LugarTrabajo;
+    private String Telefono;
+
+    public Padres(String LugarTrabajo, String Telefono, int Edad, String Nombre, String ApelldioPaterno, String ApelldioMaterno, String Direccion) {
+        super(Edad, Nombre, ApelldioPaterno, ApelldioMaterno, Direccion);
+        this.LugarTrabajo = LugarTrabajo;
+        this.Telefono = Telefono;
+    }
+
+    public Padres(String LugarTrabajo, int Edad, String Nombre, String ApelldioPaterno, String ApelldioMaterno, String Direccion) {
+        super(Edad, Nombre, ApelldioPaterno, ApelldioMaterno, Direccion);
+        this.LugarTrabajo = LugarTrabajo;
+    }
+
+    public Padres(String LugarTrabajo, String Telefono, int Edad, String Nombre, String ApelldioPaterno, String ApelldioMaterno, String Direccion, String DNI) {
+        super(Edad, Nombre, ApelldioPaterno, ApelldioMaterno, Direccion, DNI);
+        this.LugarTrabajo = LugarTrabajo;
+        this.Telefono = Telefono;
+    }
+
+    public Padres(String LugarTrabajo, int Edad, String Nombre, String ApelldioPaterno, String ApelldioMaterno, String Direccion, String DNI) {
+        super(Edad, Nombre, ApelldioPaterno, ApelldioMaterno, Direccion, DNI);
+        this.LugarTrabajo = LugarTrabajo;
+    }
+
+    public String getLugarTrabajo() {
+        return LugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String LugarTrabajo) {
+        this.LugarTrabajo = LugarTrabajo;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
     
 }
